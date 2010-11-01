52fb94cb08efbface54cd03991b7c1cdc9b77bf3
Alexandre Dupas <alexandre.dupas@gmail.com>
2010-11-01 15:30:26 +0100

    Add command to remove trailing space and double space in the typo script

--- utils/typo.sh
+++ utils/typo.sh
@@ -1,25 +1,31 @@
 #!/bin/sh
-
-#Author: Romain Goffe
+#
+#Author: Romain Goffe and Alexandre Dupas
 #Date: 27/10/2010
-#Description: apply typo rules depending on language for songs (.sg files)
-#Commentary: to be merge with latex-preprocessing script
+#Description: fix typographic mistakes, some depending on language
 
-for song in $@; do
-    echo $song
+# remove trailing space and double space
+sed -i \
+    -e 's/\s*$//g' \
+    -e '/\s*%/! s/\([^ ]\)\s\+/\1 /g' \
+    $@
+
+# formating rules depending on language
+for song in $@; 
+do
     if grep -q "selectlanguage{english}" $song
     then
 	sed -i \
             -e 's/\s*?/?/g' \
             -e 's/\s*!/!/g' \
             -e 's/\s*:/:/g' \
-            $song;
+            $song
     elif grep -q "selectlanguage{french}" $song
     then
 	sed -i \
             -e 's/\([^ ]\)?/\1 ?/g' \
             -e 's/\([^ ]\)!/\1 !/g' \
             -e '/\\gtab.*/ ! s/\([^ ]\):/\1 :/g' \
-            $song;
-    fi;
-done; 
+            $song
+    fi
+done

2b6fba6e20c7e24f380e97a56f877fcac2af9388
Romain Goffe <romain.goffe@gmail.com>
2012-01-12 01:35:15 +0100

    integrate new-songs-list within release process switch on patacrep_<version> tags to obtain the list of new songs

--- utils/new-songs-list.sh
+++ utils/new-songs-list.sh
@@ -3,13 +3,18 @@
 # Helper script to get the list of new songs added since the last version
 # The output is supposed to be copy/pasted into the NEWS item.
 
+GREP="$GREP_OPTIONS"
+export GREP_OPTIONS=""
+
 if [ $# -eq 1 ]
 then
     VERSION=$1
 else
     # Get current version
-    VERSION=`git tag | tail -n1`
+    VERSION=`git tag | grep patacrep | tail -n1`
 fi
 
 # Make new songs list by authors
 git shortlog $VERSION..master | egrep '^([^ ].*|.*Add song.*)' | sed 's/Add song.*://' | sed 's/\.$//' | sed 's/ (.*)//' | sed 's/\s\s\s*/  /'
+
+export GREP_OPTIONS="$GREP"

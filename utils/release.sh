f3d75e9b349d4532da8795c4d1d23ef3d5451300
olivier desrobert <olivier_desrobert@yahoo.fr>
2012-05-13 20:20:36 +0200

    Merge branch master lohrun

--- utils/release.sh
+++ utils/release.sh
@@ -53,18 +53,17 @@ fi;
 
 #build all songbooks
 rm -f *.d 
-make cleanall 
-make -j 4 naheulbeuk.pdf 
-make -j 4 volume-1.pdf 
-make -j 4 volume-2.pdf 
-make -j 4 volume-3.pdf 
-make -j 4 volume-4.pdf 
-make -j 4 volume-5.pdf 
-make -j 4 english.pdf 
-make -j 4 french.pdf 
-make -j 4 songbook.pdf 
-make -j 4 lyricbook.pdf 
-make -j 4 archive
+make archive
+make naheulbeuk.pdf 
+make volume-1.pdf 
+make volume-2.pdf 
+make volume-3.pdf 
+make volume-4.pdf 
+make volume-5.pdf 
+make english.pdf 
+make french.pdf 
+make songbook.pdf 
+make lyricbook.pdf 
 
 git status 
 
@@ -73,7 +72,7 @@ then
     ./utils/new-songs-list.sh | cat - NEWS > /tmp/out && mv -f /tmp/out NEWS
     echo "\nversion $MAIN.$MAJOR.$MINOR\n" | cat - NEWS > /tmp/out && mv -f /tmp/out NEWS
     git add templates/*.tmpl
-    git add books/*.sb NEWS
+    git add books/naheulbeuk.sb books/volume*.sb NEWS
     git commit -m "patacrep release version $MAIN.$MAJOR.$MINOR" 
     git tag "patacrep_$MAIN.$MAJOR.$MINOR"
 fi

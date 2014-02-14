155fab382d539d9f28f88585e2a7a7128d79e96f
Louis <spalax@gresille.org>
2014-02-14 18:07:30 +0100

    Suppression de fichiers obsolètes

--- windows/clean.bat
+++
@@ -1,4 +0,0 @@
-Rem manually performs "make clean"
-FOR %%A IN (*.aux *.d *.toc *.out *.log *.nav *.snm *.sbx *.sxd) DO DEL %%A
-Rem remove temporary covers
-FOR %%A IN (cache\songbook\*.jpg) DO DEL %%A

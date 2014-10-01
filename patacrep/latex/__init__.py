04f2554759e0eb5e30645cdc8c89649922981699
Louis <spalax@gresille.org>
2014-10-01 13:16:58 +0200

    Simplification

--- patacrep/latex/__init__.py
+++ patacrep/latex/__init__.py
@@ -7,17 +7,9 @@ This module uses an LALR parser to try to parse LaTeX code. LaTeX language
 will work on simple cases, but not on complex ones.
 """
 
-from patacrep.latex.syntax import tex2plain as syntax_tex2plain
+from patacrep.latex.syntax import tex2plain
 from patacrep.latex.syntax import parsesong as syntax_parsesong
-from patacrep.latex.detex import detex
 from patacrep import encoding
-
-def tex2plain(string):
-    """Render LaTeX string
-
-    Very few commands (mostly diacritics) are interpreted.
-    """
-    return syntax_tex2plain(string)
 
 def parsesong(path):
     """Return a dictonary of data read from the latex file `path`.

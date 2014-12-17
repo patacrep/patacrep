970f5761bcba08cf74a011b20f1138d2ac0aeb9b
Luthaf <luthaf@luthaf.fr>
2014-12-17 13:45:57 +0100

    Check for Python version at runtime
    See http://www.patacrep.com/forum/viewtopic.php?id=137
    for the error.

--- patacrep/__init__.py
+++ patacrep/__init__.py
@@ -3,8 +3,13 @@
 from pkg_resources import resource_filename
 import os
 
-# Version
+# Check Python version
+import sys
+if sys.version_info < (3, 3):
+    print("ERROR: Your Python version is too old. Please use a Python version > 3.3.")
+    sys.exit(1)
 
+# Patacrep version.
 __TUPLE_VERSION__ = (4, 0, 0, "alpha")
 __version__ = '.'.join([str(number) for number in __TUPLE_VERSION__])
 

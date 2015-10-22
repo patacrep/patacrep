093a9c4ddf43508fc5d19286ba0916c1dc8d2e73
Oliverpool <oliverpool@hotmail.fr>
2015-10-22 09:39:29 +0200

    Support define_special directive in chordpro #84

--- patacrep/songs/chordpro/ast.py
+++ patacrep/songs/chordpro/ast.py
@@ -334,11 +334,12 @@ class Define(Directive):
         open). Can be `None` if not defined.
     """
 
-    def __init__(self, key, basefret, frets, fingers):
+    def __init__(self, key, basefret, frets, fingers, is_special):
         self.key = key
         self.basefret = basefret # Can be None
         self.frets = frets
         self.fingers = fingers # Can be None
+        self.is_special = is_special
         super().__init__("define", None)
 
     @property

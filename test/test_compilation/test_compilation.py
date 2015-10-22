b1d7fa7a925b3f36ef3d7305c52b04241b3e9c62
Oliverpool <oliverpool@hotmail.fr>
2015-10-22 15:25:02 +0200

    Use a placeholder for the test folder

--- test/test_compilation/test_compilation.py
+++ test/test_compilation/test_compilation.py
@@ -89,3 +89,9 @@ class FileTest(unittest.TestCase, metaclass=dynamic.DynamicTest):
             cwd=os.path.dirname(songbook),
             )
 
+    def assertMultiLineEqual(self, result, expected):
+        """Replace the paths with the local paths"""
+        placeholder = "@TEST_FOLDER@"
+        localpath = os.path.dirname(__file__)
+        expected = expected.replace(placeholder, localpath)
+        return super().assertMultiLineEqual(result, expected)

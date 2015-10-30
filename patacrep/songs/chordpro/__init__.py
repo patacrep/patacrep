9c895c972154456ee700c69dc456399609a5ff4c
Oliverpool <oliverpool@hotmail.fr>
2015-10-30 13:17:49 +0100

    Use path2posix on LaTeX path

--- patacrep/songs/chordpro/__init__.py
+++ patacrep/songs/chordpro/__init__.py
@@ -10,6 +10,7 @@ from patacrep.songs import Song
 from patacrep.songs.chordpro.syntax import parse_song
 from patacrep.templates import Renderer
 from patacrep.latex import lang2babel
+from patacrep.files import path2posix
 
 LOGGER = logging.getLogger(__name__)
 
@@ -53,6 +54,7 @@ class ChordproSong(Song):
         jinjaenv.filters['search_image'] = self.search_image
         jinjaenv.filters['search_partition'] = self.search_partition
         jinjaenv.filters['lang2babel'] = lang2babel
+        jinjaenv.filters['path2posix'] = path2posix
 
         try:
             return Renderer(

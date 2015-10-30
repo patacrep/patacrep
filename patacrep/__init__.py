f30b8b647b66e3fe28675226be8e70afdb3c7936
Louis <spalax@gresille.org>
2015-10-30 01:38:05 +0100

    DEBUG

--- patacrep/__init__.py
+++ patacrep/__init__.py
@@ -22,3 +22,12 @@ def pkg_datapath(path=''):
     return os.path.join(_ROOT, 'data', path)
 
 __DATADIR__ = os.path.abspath(pkg_datapath())
+
+### DEBUG ###
+import subprocess
+print("DEBUG: RESOURCE_FILENAME:", os.path.abspath(resource_filename(__name__, 'data')))
+print("DEBUG: DIR RESOURCE_FILENAME:", subprocess.check_output(
+                    ['dir', os.path.abspath(resource_filename(__name__, 'data'))],
+                    stderr=subprocess.STDOUT,
+                    universal_newlines=True
+                ))

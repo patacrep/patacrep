79fa3673c6bb0eed76e8fb8f869ed02a06a768bd
Oliverpool <oliverpool@hotmail.fr>
2015-11-01 21:38:05 +0100

    Fix AppVeyor lualatex comilation

--- test/test_compilation/test_compilation.py
+++ test/test_compilation/test_compilation.py
@@ -96,8 +96,8 @@ class FileTest(unittest.TestCase, metaclass=dynamic.DynamicTest):
     @classmethod
     def _create_compilation_test(cls, base):
         """Return a function testing that `base.tex` is correctly compiled."""
-        @unittest.skipIf('TRAVIS' in os.environ or 'APPVEYOR' in os.environ,
-                         "Travis and AppVeyor do not support lualatex compilation yet")
+        @unittest.skipIf('TRAVIS' in os.environ,
+                         "Travis does not support lualatex compilation yet")
         def test_compilation(self):
             """Test that `base` is rendered to pdf."""
             # Check compilation

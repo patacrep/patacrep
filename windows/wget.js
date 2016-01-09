4065370bb0ca123afff547111090dfd1c3e3ba35
Louis <spalax@gresille.org>
2016-01-09 19:28:35 +0100

    Remove obsolete directory
    Closes #159

--- windows/wget.js
+++
@@ -1,4 +0,0 @@
-var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false);
-WinHttpReq.Send();
-WScript.Echo(WinHttpReq.ResponseText);

a655f1584b354b15da54c10f6fbacb6af09067dd
Oliverpool <oliverpool@hotmail.fr>
2016-07-03 22:38:06 +0200

    simplify custom plugin

--- test/test_content/datadir/python/content/customplugin.py
+++ test/test_content/datadir/python/content/customplugin.py
@@ -5,16 +5,10 @@ from patacrep.content import ContentItem, ContentList, validate_parser_argument
 class FakeContent(ContentItem):
     """Fake content."""
 
-    def render(self, __context):
-        return 'fakecontent'
-
     def file_entry(self):
         return {'customname:':''}
 
-#pylint: disable=unused-argument
 def parse(keyword, argument, config):
-    """
-    """
     return ContentList([FakeContent()])
 
 CONTENT_PLUGINS = {'customname': parse}
